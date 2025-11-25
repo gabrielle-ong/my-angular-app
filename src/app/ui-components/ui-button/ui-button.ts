b27b7cda95704c52f2960fa83b269f366538e9ac
Gabrielle Ong <gabbyong@gmail.com>
2025-11-25 11:26:23 +0800

    Updated UI button properties

--- src/app/ui-components/ui-button/ui-button.ts
+++ src/app/ui-components/ui-button/ui-button.ts
@@ -1,61 +1,159 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'ui-button',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   template: `
-    @if (variant === 'raised') {
+    @if (type === 'basic') {
       <button
-        mat-raised-button
         [color]="color"
         [disabled]="disabled"
-        [type]="type"
+        [type]="buttonType"
+        [attr.aria-label]="ariaLabel || (showLabel ? null : label)"
+        [class.full-width]="fullWidth"
         (click)="onClick()"
       >
-        {{ label }}
+        @if (iconPosition === 'left' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        @if (showLabel && label) {
+          <span>{{ label }}</span>
+        }
+        @if (iconPosition === 'right' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
       </button>
-    } @else if (variant === 'flat') {
+    } @else if (type === 'flat') {
       <button
         mat-flat-button
         [color]="color"
         [disabled]="disabled"
-        [type]="type"
+        [type]="buttonType"
+        [attr.aria-label]="ariaLabel || (showLabel ? null : label)"
+        [class.full-width]="fullWidth"
         (click)="onClick()"
       >
-        {{ label }}
+        @if (iconPosition === 'left' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        @if (showLabel && label) {
+          <span>{{ label }}</span>
+        }
+        @if (iconPosition === 'right' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
       </button>
-    } @else if (variant === 'stroked') {
+    } @else if (type === 'raised') {
+      <button
+        mat-raised-button
+        [color]="color"
+        [disabled]="disabled"
+        [type]="buttonType"
+        [attr.aria-label]="ariaLabel || (showLabel ? null : label)"
+        [class.full-width]="fullWidth"
+        (click)="onClick()"
+      >
+        @if (iconPosition === 'left' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        @if (showLabel && label) {
+          <span>{{ label }}</span>
+        }
+        @if (iconPosition === 'right' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+      </button>
+    } @else if (type === 'stroked') {
       <button
         mat-stroked-button
         [color]="color"
         [disabled]="disabled"
-        [type]="type"
+        [type]="buttonType"
+        [attr.aria-label]="ariaLabel || (showLabel ? null : label)"
+        [class.full-width]="fullWidth"
         (click)="onClick()"
       >
-        {{ label }}
+        @if (iconPosition === 'left' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        @if (showLabel && label) {
+          <span>{{ label }}</span>
+        }
+        @if (iconPosition === 'right' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
       </button>
-    } @else {
+    } @else if (type === 'icon') {
       <button
         mat-icon-button
         [color]="color"
         [disabled]="disabled"
-        [type]="type"
+        [type]="buttonType"
+        [attr.aria-label]="ariaLabel || label"
+        [class.full-width]="fullWidth"
         (click)="onClick()"
       >
-        {{ label }}
+        @if (icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        @if (showLabel && label) {
+          <span>{{ label }}</span>
+        }
+      </button>
+    } @else if (type === 'fab') {
+      <button
+        mat-fab
+        [color]="color"
+        [disabled]="disabled"
+        [type]="buttonType"
+        [attr.aria-label]="ariaLabel || label"
+        (click)="onClick()"
+      >
+        @if (icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        @if (showLabel && label) {
+          <span>{{ label }}</span>
+        }
+      </button>
+    } @else if (type === 'Extended') {
+      <button
+        mat-fab
+        extended
+        [color]="color"
+        [disabled]="disabled"
+        [type]="buttonType"
+        [attr.aria-label]="ariaLabel || (showLabel ? null : label)"
+        (click)="onClick()"
+      >
+        @if (iconPosition === 'left' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
+        @if (showLabel && label) {
+          <span>{{ label }}</span>
+        }
+        @if (iconPosition === 'right' && icon) {
+          <mat-icon>{{ icon }}</mat-icon>
+        }
       </button>
     }
   `,
   styleUrl: './ui-button.css'
 })
 export class UiButtonComponent {
-  @Input() label: string = '';
-  @Input() variant: 'raised' | 'flat' | 'stroked' | 'icon' = 'raised';
-  @Input() color: 'primary' | 'accent' | 'warn' = 'primary';
+  @Input() type: 'basic' | 'flat' | 'raised' | 'stroked' | 'icon' | 'fab' | 'Extended' = 'raised';
   @Input() disabled: boolean = false;
-  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() iconPosition: 'left' | 'right' | 'none' = 'none';
+  @Input() fullWidth: boolean = false;
+  @Input() label: string = '';
+  @Input() showLabel: boolean = true;
+  @Input() ariaLabel: string = '';
+  @Input() icon: string = '';
+  @Input() buttonType: 'button' | 'submit' | 'reset' = 'button';
+  @Input() color: 'primary' | 'accent' | 'warn' = 'primary';
   @Output() click = new EventEmitter<void>();
 
   onClick(): void {
